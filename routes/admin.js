c4e80c689fba8c799f35916307826e85529b0017
Lordjago <adegokeabdulazeez653@gmail.com>
2021-10-22 14:15:08 +0100

    make it better

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const isAuth = require('../middleware/is-auth');
 
 // const adminController = require('../controller/admin');
-const adminController = require('../controller/admin');
+// const adminController = require('../controller/admin')
 
 const { check } = require("express-validator");
 
@@ -11,40 +11,40 @@ const router = express.Router();
 // const products = [];
 
 
-// admin/add-product => GET
-router.get('/add-product',  isAuth, adminController.getAddProduct);
+// // admin/add-product => GET
+// router.get('/add-product',  isAuth, adminController.getAddProduct);
 
-// //admin/product-list => GET
-router.get('/product-list', isAuth, adminController.getAdminProductList);
+// // //admin/product-list => GET
+// router.get('/product-list', isAuth, adminController.getAdminProductList);
 
-// //admin/product => POST
-//use work for both get and past. we posting into /product so we change the user to post and it its get request we chnage it to get
-router.post('/add-product',[
-    check('title')
-        .isString()
-        .isLength({ min: 3 })
-        .trim(),
-    check('price').isFloat(),
-    check('description')
-        .isLength({ min: 5, max: 400 })
-        .trim()
-], isAuth, adminController.postAddProduct);
-// //Edit product
-router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
-// // 
-router.post('/edit-product', [
-    check('title')
-        .isString()
-        .isLength({ min: 3 })
-        .trim(),
-    check('price').isFloat(),
-    check('description')
-        .isLength({ min: 5, max: 400 })
-        .trim()
-], isAuth, adminController.postEditProduct);
+// // //admin/product => POST
+// //use work for both get and past. we posting into /product so we change the user to post and it its get request we chnage it to get
+// router.post('/add-product',[
+//     check('title')
+//         .isString()
+//         .isLength({ min: 3 })
+//         .trim(),
+//     check('price').isFloat(),
+//     check('description')
+//         .isLength({ min: 5, max: 400 })
+//         .trim()
+// ], isAuth, adminController.postAddProduct);
+// // //Edit product
+// router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+// // // 
+// router.post('/edit-product', [
+//     check('title')
+//         .isString()
+//         .isLength({ min: 3 })
+//         .trim(),
+//     check('price').isFloat(),
+//     check('description')
+//         .isLength({ min: 5, max: 400 })
+//         .trim()
+// ], isAuth, adminController.postEditProduct);
 
-// //delete
-router.delete('/product/:productId', isAuth, adminController.deleteProduct);
+// // //delete
+// router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 // router.get('/product', adminController.getAdminProductList);
 
