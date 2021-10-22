67485daffa7992df0411d3305a31808cfd683b15
Lordjago <adegokeabdulazeez653@gmail.com>
2021-10-22 14:39:04 +0100

    make it better

--- app.js
+++ app.js
@@ -109,45 +109,8 @@ app.use((req, res, next) => {
 })
 
 app.use(flash())
-const Product = require('./model/productModel');
-const ITEMS_PER_PAGE = 3
-app.use('/', (req, res, next) => {
-    //get page number
-    const page = +req.query.page || 1
-    let totalItems;
-    //Count the number of all product to make dynamic pagination link
-    Product.find()
-        .countDocuments()
-        .then((numProduct) => {
-            totalItems = numProduct;
-            return Product.find()
-                .skip((page - 1) * ITEMS_PER_PAGE)
-                .limit(ITEMS_PER_PAGE)
-        })
-        .then(products => {
-            // console.log(products);
-            res.render('shop/index', {
-                prods: products,
-                pageTitle: 'My Shop',
-                path: '/index',
-                hasProducts: products.length > 0,
-                currentPage: page,
-                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-                hasPrevious: page > 1,
-                nextPage: page + 1,
-                prevPage: page - 1,
-                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-            });
-        })
-        .catch((err) => {
-            const error = new Error(err);
-            error.httpStatusCode = 500
-            return next(error);
-            // console.log("Hello");
-        });
-} );
 
-// app.use('/admin', adminRoutes); // /admin was added to make only admin go this route
+app.use('/admin', adminRoutes); // /admin was added to make only admin go this route
 
 // app.use(shopRoutes);
 
